5ef51127a23edd09c622f94165fdda672c4e509c
Mathias <math649ivigtig@gmail.com>
2024-04-16 11:36:41 +0200

    Initial commit

---
+++ src/main/java/com/example/katteklubprojekt/KatteKlubProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.katteklubprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatteKlubProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatteKlubProjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/katteklubprojekt/KatteKlubProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.katteklubprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatteKlubProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
