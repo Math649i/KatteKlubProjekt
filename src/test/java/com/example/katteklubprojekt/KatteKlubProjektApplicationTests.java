9f0475ee6271011cda89602ec25ab5ea34dfb07a
Mathias <math649ivigtig@gmail.com>
2024-04-17 20:32:28 +0200

    mapper

--- src/test/java/com/example/katteklubprojekt/KatteKlubProjektApplicationTests.java
+++ src/test/java/com/example/katteklubprojekt/KatteKlubProjektApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class KatteKlubProjektApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
