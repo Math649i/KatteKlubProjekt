45125f8fb85d14dbaad06cf6ed4ba9e207b22c91
Lauge Maar <lam002@edu.zealand.dk>
2024-04-17 21:58:29 +0200

    Hej

--- src/main/java/com/example/katteklubprojekt/ControllerLayer/HomeController.java
+++ src/main/java/com/example/katteklubprojekt/ControllerLayer/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "home";
+        return "home";  // Ensure that this matches the name of your home HTML file
     }
 
     @GetMapping("/signup")
---
+++ src/main/java/com/example/katteklubprojekt/Utils/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package com.example.katteklubprojekt.Utils;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()  // Allow everyone to access the home page
+                .anyRequest().authenticated()  // All other requests need authentication
+                .and()
+                .formLogin()
+                .loginPage("/login")  // Custom login page
+                .permitAll()  // Allow everyone to see the login page
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("{noop}password").roles("USER");
+    }
+}
